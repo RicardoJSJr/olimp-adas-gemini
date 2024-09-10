8e8abf913e92ce0d12562c4806383dabf9624686
RicardoJSJr <ricardosouzajunior02@gmail.com>
2024-09-10 11:41:43 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,16 @@
+let dados = [{
+    titulo: 'Rayssa Leal',
+    descricao: 'Rayssa Leal, a Fadinha, é a mais jovem medalhista olímpica da história do Brasil. Com seu skate, ela conquistou o mundo e inspirou uma nova geração de skatistas.',
+    link: 'https://pt.wikipedia.org/wiki/Rayssa_Leal',
+    tags: 'fada skate sk8 '
+},{
+    titulo: 'Rebeca Andrade',
+    descricao: 'Rebeca Andrade é uma ginasta brasileira que conquistou o coração dos fãs com sua determinação e talento. Ela se tornou a primeira ginasta brasileira a conquistar uma medalha de ouro olímpica.',
+    link: 'https://pt.wikipedia.org/wiki/Rebeca_Andrade',
+    tags: 'ginasta ginastica ouro '
+},   {
+    titulo: 'Beatriz Souza',
+    descricao: 'Beatriz Souza é uma judoca brasileira de destaque, conhecida por sua força e técnica. Ela conquistou diversas medalhas em competições nacionais e internacionais.',
+    link: 'https://pt.wikipedia.org/wiki/Beatriz_Souza',
+    tags: 'judo judoka ouro'
+}];
